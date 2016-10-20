91629af15e661a374d28c66f630f52505e4f71e0
krismaz <krislinux@gmail.com>
2016-10-21 00:23:06 +0200

    Split connection logic to separate files, allows for easy debugging

---
+++ DebugCommunicator.py
@@ -0,0 +1,23 @@
+from time import sleep, time
+
+
+class DebugCommunicator:
+    def __init__(self, port, sensor):
+        self.setup_serial(port)
+        self.set_sensor(sensor)
+        self.start = time()
+
+    def setup_serial(self, port):
+        print('Connecting to', port)
+        sleep(3)
+        print('Connection done!')
+
+    def set_sensor(self, sensor):
+        self.sensor = sensor
+        print('Sensor set:', sensor)
+
+    def set_temperature(self, temp):
+        print('Temperature set:', temp)
+
+    def get_temperatures(self):
+        return {self.sensor: time() - self.start}
--- SerialCom.py
+++ SerialCom.py
@@ -1,20 +1,15 @@
-import serial
-import io
 from time import sleep, time
-from struct import pack
 from pymsgbox import *
 import json
 import datetime
 import argparse
+from ArduinoCommunicator import ArduinoCommunicator
+from DebugCommunicator import DebugCommunicator
 
 parser = argparse.ArgumentParser(description='Brewing process runner.')
 parser.add_argument('-s', dest="settings", default="settings.json",
                     help="Settings file")
 args = parser.parse_args()
-
-sio = None
-
-lasttarget = ''
 
 with open(args.settings, 'r') as settingsfile:
     settings = json.load(settingsfile)
@@ -24,57 +19,7 @@ def name_sensors(raw):
     return dict((settings['names'].get(k) or k, v) for k, v in raw.items())
 
 
-def get_temperatures():
-    ser.write(bytes([1]))
-    ser.flush()
-    res = dict()
-    for i in range(int(sio.readline())):
-        addr = sio.readline().strip()  # Right side evaluates first
-        res[addr] = float(sio.readline())
-    return res
-
-
-def set_temperatures(temp):
-    bts = pack('f', temp)
-    ser.write(bytes([2]))
-    ser.write(bts)
-    ser.flush()
-    print('Target set:', sio.readline())
-
-
-def set_target(target):
-    global lasttarget
-    lasttarget = target
-    ser.write(bytes([3]))
-    ser.write(bytes(map(lambda x: int(x, 16), target.split(" "))))
-    ser.flush()
-    print('Temp set:', sio.readline())
-
-
-def setup_serial(port):
-    print('a')
-    global sio, ser
-    try:
-        ser = serial.Serial(
-            port=port,
-            baudrate=9600,
-            timeout=3
-        )
-    except serial.serialutil.SerialException:
-        print('Error communicating with device "{}"! \n'.format(port) +
-              'Have you provided the correct COM port?')
-        exit(1)
-    print('a')
-    sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser, 1),
-                           encoding='ascii',
-                           newline=None)
-    sio._CHUNK_SIZE = 1
-    print('a')
-    sleep(10)
-    print('a')
-
-
-def evaluate(command, index):
+def evaluate(command, index, coms):
     if not command:
         return
     print(command, '----')
@@ -84,26 +29,26 @@ def evaluate(command, index):
     if op == 'TARGET':
         print('TARGET command deprecated, use settings file.')
     if op == 'HEAT':
-        set_temperatures(float(args[0]))
+        coms.set_temperature(float(args[0]))
         while True:
-            temps = get_temperatures()
+            temps = coms.get_temperatures()
             print(index, '-', name_sensors(temps))
             try:
-                if temps[lasttarget] >= float(args[0]):
+                if temps[coms.sensor] >= float(args[0]):
                     break
             except KeyError:
-                print('Unknown target sensor {}!'.format(lasttarget))
+                print('Unknown target sensor {}!'.format(coms.sensor))
                 print('Connected sensors are:')
                 print('\n'.join(*temps.keys()))
                 exit(1)
             sleep(5)
     if op == 'COOK':
         start = time()
-        set_temperatures(float(args[0]))
+        coms.set_temperature(float(args[0]))
         while True:
-            temps = get_temperatures()
+            temps = coms.get_temperatures()
             print(index, '-', name_sensors(temps))
-            remaining = start + float(args[1])*60.0 - time()
+            remaining = start + float(args[1]) * 60.0 - time()
             if remaining < 0:
                 break
             print('Time remaining:', datetime.timedelta(seconds=remaining))
@@ -111,14 +56,14 @@ def evaluate(command, index):
     if op == 'PAUSE':
         alert(text=' '.join(args), title='', button='OK')
     if op == 'DONE':
-        set_temperatures(-100000000.0)
-        set_target('0x0 0x0 0x0 0x0 0x0 0x0 0x0 0x0')
+        coms.set_temperature(-100000000.0)
+        coms.set_target('0x0 0x0 0x0 0x0 0x0 0x0 0x0 0x0')
 
-i = 1
 
 if __name__ == "__main__":
-    setup_serial(settings['COM'])
-    set_target(settings['sensor'])
+    i = 1
+    # coms = ArduinoCommunicator(settings['COM'], settings['sensor'])
+    coms = DebugCommunicator(settings['COM'], settings['sensor'])
     while(True):
-        evaluate(input().strip(), i)
+        evaluate(input().strip(), i, coms)
         i += 1
