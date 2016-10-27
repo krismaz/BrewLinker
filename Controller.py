ff38b3fc524d6d5d2b18927682354cc12e1e42bf
krismaz <krislinux@gmail.com>
2016-10-27 10:53:31 +0200

    QT testing GUI!!!!

---
+++ Controller.py
@@ -0,0 +1,64 @@
+from time import sleep, time
+import datetime
+from pymsgbox import *
+import threading
+from pubsub import pub
+
+
+class Controller:
+    def __init__(self, settings, coms):
+        self.settings = settings
+        self.coms = coms
+        thread = threading.Thread(target=self.brew_loop)
+        thread.start()
+
+    def name_sensors(self, raw):
+        return dict((self.settings['names'].get(k) or k, v) for k, v in raw.items())
+
+    def evaluate(self, command, index):
+        if not command:
+            return
+        print(command, '----')
+        if command[0] == '#':
+            return
+        op, *args = command.split(' ')
+        if op == 'TARGET':
+            print('TARGET command deprecated, use settings file.')
+        if op == 'HEAT':
+            self.coms.set_temperature(float(args[0]))
+            while True:
+                temps = self.coms.get_temperatures()
+                print(index, '-', self.name_sensors(temps))
+                try:
+                    if temps[self.coms.sensor] >= float(args[0]):
+                        break
+                except KeyError:
+                    print('Unknown target sensor {}!'.format(self.coms.sensor))
+                    print('Connected sensors are:')
+                    print('\n'.join(*temps.keys()))
+                    exit(1)
+                pub.sendMessage('MainTemp', arg1=temps[self.coms.sensor])
+                sleep(5)
+        if op == 'COOK':
+            start = time()
+            coms.set_temperature(float(args[0]))
+            while True:
+                temps = self.coms.get_temperatures()
+                print(index, '-', self.name_sensors(temps))
+                remaining = start + float(args[1]) * 60.0 - time()
+                if remaining < 0:
+                    break
+                print('Time remaining:', datetime.timedelta(seconds=remaining))
+                pub.sendMessage('MainTemp', arg1=temps[self.coms.sensor])
+                sleep(5)
+        if op == 'PAUSE':
+            alert(text=' '.join(args), title='', button='OK')
+        if op == 'DONE':
+            self.coms.set_temperature(-100000000.0)
+            self.coms.set_target('0x0 0x0 0x0 0x0 0x0 0x0 0x0 0x0')
+
+    def brew_loop(self):
+        i = 1
+        while(True):
+            self.evaluate(input().strip(), i)
+            i += 1
--- SerialCom.py
+++ SerialCom.py
@@ -1,69 +1,38 @@
-from time import sleep, time
-from pymsgbox import *
 import json
-import datetime
 import argparse
 from ArduinoCommunicator import ArduinoCommunicator
 from DebugCommunicator import DebugCommunicator
-
-parser = argparse.ArgumentParser(description='Brewing process runner.')
-parser.add_argument('-s', dest="settings", default="settings.json",
-                    help="Settings file")
-args = parser.parse_args()
-
-with open(args.settings, 'r') as settingsfile:
-    settings = json.load(settingsfile)
+import gui
+from PyQt5 import QtWidgets
+from Controller import Controller
+import sys
+from pubsub import pub
 
 
-def name_sensors(raw):
-    return dict((settings['names'].get(k) or k, v) for k, v in raw.items())
-
-
-def evaluate(command, index, coms):
-    if not command:
-        return
-    print(command, '----')
-    if command[0] == '#':
-        return
-    op, *args = command.split(' ')
-    if op == 'TARGET':
-        print('TARGET command deprecated, use settings file.')
-    if op == 'HEAT':
-        coms.set_temperature(float(args[0]))
-        while True:
-            temps = coms.get_temperatures()
-            print(index, '-', name_sensors(temps))
-            try:
-                if temps[coms.sensor] >= float(args[0]):
-                    break
-            except KeyError:
-                print('Unknown target sensor {}!'.format(coms.sensor))
-                print('Connected sensors are:')
-                print('\n'.join(*temps.keys()))
-                exit(1)
-            sleep(5)
-    if op == 'COOK':
-        start = time()
-        coms.set_temperature(float(args[0]))
-        while True:
-            temps = coms.get_temperatures()
-            print(index, '-', name_sensors(temps))
-            remaining = start + float(args[1]) * 60.0 - time()
-            if remaining < 0:
-                break
-            print('Time remaining:', datetime.timedelta(seconds=remaining))
-            sleep(5)
-    if op == 'PAUSE':
-        alert(text=' '.join(args), title='', button='OK')
-    if op == 'DONE':
-        coms.set_temperature(-100000000.0)
-        coms.set_target('0x0 0x0 0x0 0x0 0x0 0x0 0x0 0x0')
-
+def update_temp(arg1):
+    ui.progressBar.setProperty('value', arg1)
 
 if __name__ == "__main__":
-    i = 1
-    coms = ArduinoCommunicator(settings['COM'], settings['sensor'])
-    # coms = DebugCommunicator(settings['COM'], settings['sensor'])
-    while(True):
-        evaluate(input().strip(), i, coms)
-        i += 1
+    global ui
+    parser = argparse.ArgumentParser(description='Brewing process runner.')
+    parser.add_argument('-s', dest="settings", default="settings.json",
+                        help="Settings file")
+    args = parser.parse_args()
+
+    with open(args.settings, 'r') as settingsfile:
+        settings = json.load(settingsfile)
+
+    # coms = ArduinoCommunicator(settings['COM'], settings['sensor'])
+    coms = DebugCommunicator(settings['COM'], settings['sensor'])
+
+    control = Controller(settings, coms)
+
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = gui.Ui_MainWindow()
+    ui.setupUi(MainWindow)
+
+    pub.subscribe(update_temp, 'MainTemp')
+
+    MainWindow.show()
+    sys.exit(app.exec_())
