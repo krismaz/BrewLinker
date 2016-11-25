ef91a4358dc57bab1f87e3d9783246f0d956aaf6
krismaz <krislinux@gmail.com>
2016-11-26 00:45:48 +0100

    Ups

--- ArduinoCommunicator.py
+++ ArduinoCommunicator.py
@@ -2,10 +2,11 @@ import serial
 import io
 from time import sleep
 from struct import pack
-
+from threading import Lock
 
 class ArduinoCommunicator:
     def __init__(self, port, sensor):
+        self.lock = Lock()
         self.setup_serial(port)
         self.set_sensor(sensor)
 
@@ -32,32 +33,42 @@ class ArduinoCommunicator:
         self.ser, self.sio = ser, sio
 
     def set_sensor(self, sensor):
+        self.lock.acquire()
         self.sensor = sensor
         self.ser.write(bytes([3]))
         self.ser.write(bytes(map(lambda x: int(x, 16), sensor.split(" "))))
         self.ser.flush()
         print('Sensor set:', self.sio.readline())
+        self.lock.release()
 
     def set_temperature(self, temp):
+        self.lock.acquire()
         bts = pack('f', temp)
         self.ser.write(bytes([2]))
         self.ser.write(bts)
         self.ser.flush()
         print('Temperature set:', self.sio.readline())
+        self.lock.release()
 
     def get_temperatures(self):
+        self.lock.acquire()
         self.ser.write(bytes([1]))
         self.ser.flush()
         res = dict()
         for i in range(int(self.sio.readline())):
             addr = self.sio.readline().strip()  # Right side evaluates first
             res[addr] = float(self.sio.readline())
+        self.lock.release()
         return res
 
     def pump_on(self):
+        self.lock.acquire()
         self.ser.write(bytes([4]))
         self.ser.flush()
+        self.lock.release()
 
     def pump_off(self):
+        self.lock.acquire()
         self.ser.write(bytes([5]))
         self.ser.flush()
+        self.lock.release()
