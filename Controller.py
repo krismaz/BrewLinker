281dfca6f4b2bcc51f7bb029576ac13e4204d3a9
Bryggeri <krislinux@gmail.com>
2016-11-26 00:40:45 +0100

    moving files

--- Controller.py
+++ Controller.py
@@ -7,8 +7,10 @@ from PyQt5.QtCore import QThread, pyqtSignal
 
 class Controller(QThread):
     program_changed = pyqtSignal([object])
+    sensors_changed = pyqtSignal(object)
     temp_changed = pyqtSignal(float)
     pump_changed = pyqtSignal(bool)
+
 
     def __init__(self, settings, coms, script, parent=None):
         QThread.__init__(self, parent)
@@ -18,18 +20,21 @@ class Controller(QThread):
         self.pump = False
         self.program = [step for step in map(Steps.parse, script) if step]
         self.current_step = None
+        self.to_pause = False
 
     def name_sensors(self, raw):
         return dict((self.settings['names'].get(k) or k, v) for k, v in raw.items())
 
     def evaluate(self, op, index):
         self.program_changed.emit(self.program)
+        self.to_pause = False
         if op.tag == 'TARGET':
             print('TARGET command deprecated, use settings file.')
         if op.tag == 'HEAT':
             self.coms.set_temperature(op.temp)
             while True:
                 temps = self.coms.get_temperatures()
+                self.sensors_changed.emit(self.name_sensors(temps))
                 print(index, '-', self.name_sensors(temps))
                 try:
                     if temps[self.coms.sensor] >= op.temp:
@@ -40,18 +45,29 @@ class Controller(QThread):
                     print('\n'.join(*temps.keys()))
                     exit(1)
                 self.temp_changed.emit(temps[self.coms.sensor])
+                if self.to_pause:
+                    self.coms.set_temperature(-100000000.0)
+                    alert(text='PAUSE', title='', button='OK')
+                    self.to_pause = False
+                    self.coms.set_temperature(op.temp)
                 sleep(5)
         if op.tag == 'COOK':
             start = time()
             self.coms.set_temperature(op.temp)
             while True:
                 temps = self.coms.get_temperatures()
+                self.sensors_changed.emit(self.name_sensors(temps))
                 print(index, '-', self.name_sensors(temps))
                 remaining = start + op.time - time()
                 if remaining < 0:
                     break
                 print('Time remaining:', datetime.timedelta(seconds=remaining))
                 self.temp_changed.emit(temps[self.coms.sensor])
+                if self.to_pause:
+                    self.coms.set_temperature(-100000000.0)
+                    alert(text='PAUSE', title='', button='OK')
+                    self.to_pause = False
+                    self.coms.set_temperature(op.temp)
                 sleep(5)
         if op.tag == 'PAUSE':
             self.coms.set_temperature(-100000000.0)
@@ -70,6 +86,18 @@ class Controller(QThread):
             self.coms.pump_on()
             self.pump = True
         self.pump_changed.emit(self.pump)
+
+    def shift_temp(self, diff):
+        try:
+            self.current_step.temp += diff
+            if not self.to_pause:
+                self.coms.set_temperature(self.current_step.temp)
+            self.program_changed.emit(self.program)
+        except Exception as e:
+            print(e)
+
+    def request_pause(self):
+        self.to_pause = True
 
     def brew_loop(self):
         for i, step in enumerate(self.program):
