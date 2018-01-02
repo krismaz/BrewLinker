8bf204d8a1f5659bfec84387fe3e2c6a7da390b7
krismaz <krislinux@gmail.com>
2018-01-02 18:11:47 +0100

    Fixes #24

--- SSR.h
+++ SSR.h
@@ -5,6 +5,13 @@
 #define SSR_PINb 9
 #define SSR_PINc 10
 
+#define LED_PINa 22
+#define LED_PINb 24
+#define LED_PINc 26
+#define LED_PINd 28
+#define LED_PINe 30
+
+
 #define PUMP_PIN 11
 
 inline void SSRInit()
@@ -30,6 +37,13 @@ inline void SSROff()
   digitalWrite(SSR_PINa, LOW);
   digitalWrite(SSR_PINb, LOW);
   digitalWrite(SSR_PINc, LOW);
+
+  digitalWrite(LED_PINa, LOW);
+  digitalWrite(LED_PINb, LOW);
+  digitalWrite(LED_PINc, LOW);
+  digitalWrite(LED_PINd, LOW);
+  digitalWrite(LED_PINe, LOW);
+
 }
 
 inline void SSRPhase(float diff)
@@ -40,16 +54,21 @@ inline void SSRPhase(float diff)
     digitalWrite(SSR_PINa, HIGH);
     digitalWrite(SSR_PINb, HIGH);
     digitalWrite(SSR_PINc, HIGH); 
+    digitalWrite(LED_PINa, LOW);
   } else if (diff > 3.0f){
     digitalWrite(SSR_PINb, HIGH);
     digitalWrite(SSR_PINc, HIGH);
+    digitalWrite(LED_PINb, LOW);
   } else if (diff > 1.5f){
     digitalWrite(SSR_PINa, HIGH);
     digitalWrite(SSR_PINb, HIGH);
+    digitalWrite(LED_PINc, LOW);
   } else if (diff > 0.5f){
     digitalWrite(SSR_PINc, HIGH);
+    digitalWrite(LED_PINd, LOW);
   } else {
     digitalWrite(SSR_PINb, HIGH);
+    digitalWrite(LED_PINe, LOW);
   }
 }
 }
